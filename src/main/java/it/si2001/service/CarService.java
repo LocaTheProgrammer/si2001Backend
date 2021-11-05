c69c14bbbfc71cee32cc0047f0a74538c11e2512
LocaTheProgrammer <pietro.locatelli@stu.fondazionejobsacademy.org>
2021-11-05 17:47:03 +0100

    update car method

--- src/main/java/it/si2001/service/CarService.java
+++ src/main/java/it/si2001/service/CarService.java
@@ -9,20 +9,17 @@ import it.si2001.entity.Car;
 
 @Service
 public class CarService {
-	
-	
+
 	private CarRepository carRepository;
-	
+
 	public CarService(CarRepository carRepository) {
-		this.carRepository=carRepository;
+		this.carRepository = carRepository;
 	}
-	
-	
+
 	public Response<CarDTO> createCar(CarDTO carDTO) {
-		
-		
-		Response<CarDTO> res= new Response<CarDTO>();
-		
+
+		Response<CarDTO> res = new Response<CarDTO>();
+
 		Car car = new Car();
 		car.setCylinders(carDTO.getCylinders());
 		car.setDisplacement(carDTO.getDisplacement());
@@ -30,47 +27,86 @@ public class CarService {
 		car.setMilesPerGallon(carDTO.getMilesPerGallon());
 		car.setName(carDTO.getName());
 		car.setWeightInLbs(carDTO.getWeightInLbs());
-		
-		try {			
+
+		try {
 			this.carRepository.save(car);
 			res.setResult(carDTO);
 			res.setResultTest(true);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			res.setError("car not saved");
 		}
-		
+
 		return res;
-		
+
 	}
-	
-	
-	public Response<CarDTO> findCarById(int id){
-		
-		Response<CarDTO> res=new Response<CarDTO>();
-		
+
+	public Response<CarDTO> findCarById(int id) {
+
+		Response<CarDTO> res = new Response<CarDTO>();
+
 		try {
-			Car c=this.carRepository.findById(id).get();
+			Car c = this.carRepository.findById(id).get();
 			res.setResult(CarDTO.build(c));
 			res.setResultTest(true);
-			
-		}catch(Exception e) {
-			res.setError("no car found for id: "+id);
+
+		} catch (Exception e) {
+			res.setError("no car found for id: " + id);
 		}
-		
-		
+
 		return res;
 	}
-	
-	
+
 	public boolean deleteCarById(int id) {
-		
+
 		try {
 			this.carRepository.delete(this.carRepository.findById(id).get());
 			return true;
-		}catch(Exception e) {
+		} catch (Exception e) {
 			return false;
 		}
+
+	}
+
+	public Response<CarDTO> updateCar(CarDTO carDTO) {
 		
+		Response<CarDTO> res = new Response<CarDTO>();
+		
+		Car car = new Car();
+		
+		if(carDTO.getCylinders()!=null) {
+			car.setCylinders(carDTO.getCylinders());
+		}
+		
+		if(carDTO.getDisplacement()!=null) {
+			car.setDisplacement(carDTO.getDisplacement());
+		}
+		
+		if(carDTO.getHorsePower()!=null) {
+			car.setHorsePower(carDTO.getHorsePower());
+		}
+		if(carDTO.getMilesPerGallon()!=null) {
+			car.setMilesPerGallon(carDTO.getMilesPerGallon());
+		}
+		if(carDTO.getName()!=null) {
+			car.setName(carDTO.getName());
+		}
+		if(carDTO.getWeightInLbs()!=null) {
+			car.setWeightInLbs(carDTO.getWeightInLbs());
+		}
+		
+		
+		try {
+			this.carRepository.save(car);
+			
+			res.setResult(carDTO);
+			res.setResultTest(true);
+			
+		}catch(Exception e) {
+			res.setError("car not updated");
+			res.setResultTest(false);
+		}
+		
+		return res;
 	}
 
 }
