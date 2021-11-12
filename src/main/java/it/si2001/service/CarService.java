16109d0e9af3d4072000efad45e92be0a3045a6e
LocaTheProgrammer <pietro.locatelli@stu.fondazionejobsacademy.org>
2021-11-12 17:38:57 +0100

    cleaning up my code

--- src/main/java/it/si2001/service/CarService.java
+++ src/main/java/it/si2001/service/CarService.java
@@ -6,7 +6,6 @@ import it.si2001.dao.CarRepository;
 import it.si2001.dao.ReservationRepository;
 import it.si2001.dto.CarDTO;
 import it.si2001.dto.NgbDateDTO;
-import it.si2001.dto.ReservationDTO;
 import it.si2001.dto.Response;
 import it.si2001.entity.Car;
 import it.si2001.entity.Reservation;
@@ -14,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -23,7 +23,7 @@ public class CarService {
 
     private final CarRepository carRepository;
     private final ReservationRepository reservationRepository;
-    private EntityDTOConverter entityDTOConverter;
+    private final EntityDTOConverter entityDTOConverter;
 
     public CarService(CarRepository carRepository, ReservationRepository reservationRepository, EntityDTOConverter entityDTOConverter) {
         this.carRepository = carRepository;
@@ -53,9 +53,11 @@ public class CarService {
         Response<CarDTO> res = new Response<>();
 
         try {
-            Car c = this.carRepository.findById(id).get();
-            res.setResult(CarDTO.build(c));
-            res.setResultTest(true);
+            Optional<Car> c = this.carRepository.findById(id);
+            if(c.isPresent()){
+                res.setResult(CarDTO.build(c.get()));
+                res.setResultTest(true);
+            }
 
         } catch (Exception e) {
             res.setError("no car found for id: " + id);
@@ -64,75 +66,77 @@ public class CarService {
         return res;
     }
 
+    @Transactional
     public Response<Boolean> deleteCarById(int id) {
         Response<Boolean> res =new Response<>();
         try {
-            this.carRepository.delete(this.carRepository.findById(id).get());
-            if (this.reservationRepository.findAllByCarId(id).size()>0){
-                this.reservationRepository.deleteAllByCarId(id);
+            Optional<Car> c = this.carRepository.findById(id);
+            if(c.isPresent()){
+                this.carRepository.delete(c.get());
+                res.setResult(true);
+                return res;
             }
-            res.setResult(true);
-            return res;
+
         } catch (Exception e) {
             res.setResult(false);
             return res;
         }
-
+        return res;
     }
 
     public Response<CarDTO> updateCar(CarDTO carDTO) throws ParseException {
 
         Response<CarDTO> res = new Response<>();
 
-        Car car =  this.carRepository.findById(carDTO.getId()).get();
+        Optional<Car> car =  this.carRepository.findById(carDTO.getId());
 
 
 
+        if (car.isPresent()){
+            if (carDTO.getCylinders() != null) {
+                car.get().setCylinders(carDTO.getCylinders());
+            }
 
-        if (carDTO.getCylinders() != null) {
-            car.setCylinders(carDTO.getCylinders());
+            if (carDTO.getDisplacement() != null) {
+                car.get().setDisplacement(carDTO.getDisplacement());
+            }
+
+            if (carDTO.getHorsePower() != null) {
+                car.get().setHorsePower(carDTO.getHorsePower());
+            }
+            if (carDTO.getMilesPerGallon() != null) {
+                car.get().setMilesPerGallon(carDTO.getMilesPerGallon());
+            }
+            if (carDTO.getName() != null) {
+                car.get().setName(carDTO.getName());
+            }
+            if (carDTO.getWeightInLbs() != null) {
+                car.get().setWeightInLbs(carDTO.getWeightInLbs());
+            }
+
+            if (carDTO.getAcceleration() != null) {
+                car.get().setAcceleration(carDTO.getAcceleration());
+            }
+            if (carDTO.getYear() != null) {
+                car.get().setYear(this.entityDTOConverter.simpleDateFormat.parse(carDTO.getYear()));
+            }
+            if (carDTO.getOrigin() != null) {
+                car.get().setOrigin(carDTO.getOrigin());
+            }
+
+            try {
+                this.carRepository.save(car.get());
+
+                res.setResult(carDTO);
+                res.setResultTest(true);
+
+            } catch (Exception e) {
+                res.setResult(null);
+                res.setError("car not updated");
+                res.setResultTest(false);
+            }
         }
 
-        if (carDTO.getDisplacement() != null) {
-            car.setDisplacement(carDTO.getDisplacement());
-        }
-
-        if (carDTO.getHorsePower() != null) {
-            car.setHorsePower(carDTO.getHorsePower());
-        }
-        if (carDTO.getMilesPerGallon() != null) {
-            car.setMilesPerGallon(carDTO.getMilesPerGallon());
-        }
-        if (carDTO.getName() != null) {
-            car.setName(carDTO.getName());
-        }
-        if (carDTO.getWeightInLbs() != null) {
-            car.setWeightInLbs(carDTO.getWeightInLbs());
-        }
-
-        if (carDTO.getAcceleration() != null) {
-            car.setAcceleration(carDTO.getAcceleration());
-        }
-        if (carDTO.getYear() != null) {
-            car.setYear(this.entityDTOConverter.simpleDateFormat.parse(carDTO.getYear()));
-        }
-        if (carDTO.getOrigin() != null) {
-            car.setOrigin(carDTO.getOrigin());
-        }
-
-
-        log.info("updating car: "+car.toString());
-        try {
-            this.carRepository.save(car);
-
-            res.setResult(carDTO);
-            res.setResultTest(true);
-
-        } catch (Exception e) {
-            res.setResult(null);
-            res.setError("car not updated");
-            res.setResultTest(false);
-        }
 
         return res;
     }
@@ -157,9 +161,9 @@ public class CarService {
     }
 
 
-    public List<CarDTO> getFreeCarByReservationDate(NgbDateDTO fromDate, NgbDateDTO toDate) throws ParseException {
+    public List<CarDTO> getFreeCarByReservationDate(NgbDateDTO fromDate, NgbDateDTO toDate) {
 
-        List<Reservation> reservation = new ArrayList<>();
+        List<Reservation> reservation;
         reservation=this.reservationRepository.findAll();
         List<CarDTO> carDTOList = this.findAllCars();
         if (reservation.size() == 0) {
@@ -204,13 +208,6 @@ public class CarService {
             }
             return availableCarList;
         }
-    }
-
-
-    private Date getDateToCheck(String[] date, SimpleDateFormat simpleDateFormat) throws ParseException {
-        int month = Integer.parseInt(date[1]);
-        String formattedDateFromString = date[0] + "/" + month + "/" + date[2];
-        return simpleDateFormat.parse(formattedDateFromString);
     }
 
 
