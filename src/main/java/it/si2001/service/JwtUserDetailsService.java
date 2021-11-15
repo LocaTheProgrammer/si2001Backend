b08e627c1d81bf440ec36eaebf0d88067faddb03
LocaTheProgrammer <pietro.locatelli@stu.fondazionejobsacademy.org>
2021-11-15 12:52:53 +0100

    jwt token dinamico

--- src/main/java/it/si2001/service/JwtUserDetailsService.java
+++ src/main/java/it/si2001/service/JwtUserDetailsService.java
@@ -1,7 +1,9 @@
 package it.si2001.service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
+import it.si2001.dao.UserRepository;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,20 +13,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
+    public JwtUserDetailsService(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if ("user".equals(username)) {
-            return new User("user", "$2a$12$Vvqh0KO7K5ZqFydpMa0w0ec8nCS7cu11B6/uQIGw/WJrI2fNMkVKm",//password is "user"
-                    new ArrayList<>());
+        Optional<it.si2001.entity.User> u = this.userRepository.findByEmail(username);
+
+        if (u.isPresent()) {
+            if (u.get().getEmail().equals(username)) {
+                return new User(u.get().getEmail(), u.get().getPassword(),
+                        new ArrayList<>());
+            }else{
+                throw new UsernameNotFoundException("User not found with username: " + username);
+            }
         }
-        if ("admin".equals(username)) {
-            return new User("admin", "$2a$12$3g4zJNiReRDnmYJiOQ58QeCAeee1YdFQkBZWgHP614p0v1lKA.PIq",//password is "admin"
-                    new ArrayList<>());
-        }
-        if ("sarah".equals(username)) {
-            return new User("admin", "$2a$12$z9XRjFo5TbEyS9IWOInehOECIh3UFtJnM0VMsyiLACd6GQ4HPm6m6",//password is "password"
-                    new ArrayList<>());
-        }
+
         else {
             throw new UsernameNotFoundException("User not found with username: " + username);
         }
